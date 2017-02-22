c95fa0876b1b61bd035232039878d6d5157ee46d
Mauri <moere91@gmail.com>
2017-02-22 14:04:31 +0100

    Ignacios Files as seen in class
    Some functionalities are not yet included and some of the files need to
    be finished

---
+++ studentview.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 11:40:50 2017
+
+@author: Maurice
+"""
+
+import OOPSTController
+
+class view:
+    
+    def __init__(self,controller):
+        self.controller = controller
+        
+    def mainloop(self):
+        while choice != "0":
+            print(
+                  """
+                  Welcome to student manager
+                  
+                  0-Quit
+                  1-Average frade for one student
+                  2-Average of the average grade of all students
+                  3-Average grade for one course
+                  """
+                  )
+            choice = input("Input command: ")
+            print()
+            
+            if choice == "0"
